aefaa1742d147ad0c07bfe0dad0d038fe1a2e591
Md Naime Hossen <n33937407@gmail.com>
2025-10-18 15:50:22 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,8 @@ connectDB();
 
 app.use("/api/auth", adminRoutes);
 
+app.get('/',(req,res)=>{
+  res.send("Api working")
+} )
+
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
