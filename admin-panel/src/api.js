d82e40ac121334945114464627b385e834ac86fa
Md Naime Hossen <n33937407@gmail.com>
2025-10-18 15:43:25 +0600

    Update api.js

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://two-step-verification-1.onrender.com/api",
+  baseURL:"https://two-step-verification-1.onrender.com/api",
 });
 
 export default api;
